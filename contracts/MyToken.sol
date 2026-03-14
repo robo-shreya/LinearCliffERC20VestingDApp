f7a1dd7f1571e91efd8e19bae8f4e5e644e1fcb9
Shreya <srivastava.shreya987@gmail.com>
2026-03-15 00:17:14 +0100

    MyToken.sol completed

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -28,18 +28,27 @@ contract MyToken {
     mapping(address => mapping(address => uint256)) public allowance;
 
     // events
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
 
-    function transferFrom(address from, address to, uint256 value) public {
-        
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        allowance[from][msg.sender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+        return true;
     }
 
-    function approve(address owner, address spender, uint256 value) public {
-
+    function approve(address spender, uint256 amount) external returns (bool){
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
 
-    function transfer(address to, uint256 amount) public {
-
+    function transfer(address to, uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
     }
 }
